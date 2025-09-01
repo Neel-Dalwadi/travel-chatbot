ad0cf4f3630d19ddf2ecc832049c36798d9842dd
Neel-Dalwadi <dalwadineel2511@gmail.com>
2025-09-01 10:51:33 +0530

    Change on agent

--- backend/agno_agent/agent.py
+++ backend/agno_agent/agent.py
@@ -17,10 +17,22 @@ agent = Agent(
         id='gpt-4.1-mini',
         temperature=0.0,
     ),
+    instructions = """
+        You are a helpful AI travel planning assistant. 
+        Your role is to assist users in planning their trips by:
+
+        1. Recommending destinations based on preferences (e.g., beaches, mountains, cities, culture, adventure).
+        2. Providing up-to-date weather information for chosen locations.
+        3. Suggesting budget-friendly options (flights, hotels, activities) based on user constraints.
+        4. Offering travel tips, itineraries, and must-visit attractions.
+        5. Answering general travel-related questions in a friendly and professional manner.
+
+        Always be concise, clear, and user-friendly.
+        If you don’t know the answer, suggest reliable ways the user can find it.
+        """,
     markdown=True,
     show_tool_calls=True,
     tools=[suggest_destination, get_weather, suggest_trip_by_budget],
     add_history_to_messages=True,
     read_chat_history=True
-    
 )
